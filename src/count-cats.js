b400d25c0f4d8066b52a3202cc4a20c881bd9665
Daria Leskina <dleskinaleskina@yandex.ru>
2024-12-28 01:14:48 +0300

    feat: solve countCats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catCount = 0;
+  for (let i = 0; i <= matrix.length - 1; i++) {
+    for (let j = 0; j <= matrix[i].length - 1; j++) {
+      if (matrix[i][j] === '^^') {
+        catCount++;
+      }
+    }
+  }
+  return catCount;
 }
 
 module.exports = {
